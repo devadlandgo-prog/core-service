b7d58e111c05d36c11d61d918e935ac759cc0fa2
Himanshu Ray <jackofalltradesray@gmail.com>
2026-05-05 21:49:29 +0530

    fix: add missing RequiredArgsConstructor import in EnquiryService

--- src/main/java/com/landgo/coreservice/service/EnquiryService.java
+++ src/main/java/com/landgo/coreservice/service/EnquiryService.java
@@ -3,6 +3,7 @@ package com.landgo.coreservice.service;
 import com.landgo.coreservice.entity.Enquiry;
 import com.landgo.coreservice.enums.EnquiryStatus;
 import com.landgo.coreservice.repository.EnquiryRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
