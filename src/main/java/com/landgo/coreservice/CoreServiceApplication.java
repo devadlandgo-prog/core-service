330142fb54896a60afe3bd72288b3967801797c4
Himanshu Ray <jackofalltradesray@gmail.com>
2026-05-14 00:26:40 +0530

    Synchronize with monorepo state (aligned history)

--- src/main/java/com/landgo/coreservice/CoreServiceApplication.java
+++ src/main/java/com/landgo/coreservice/CoreServiceApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-@EnableAsync
-// Force re-deployment
 public class CoreServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoreServiceApplication.class, args);
